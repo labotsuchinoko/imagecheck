a4d6dd65501776c5add59f3998abe8077b82961d
oyabun <ar@tky.dooga.co.jp>
2017-06-13 06:33:20 +0000

    first commit

---
+++ api.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import cv2
+import numpy as np
+import json
+import urllib2
+import os
+import urllib
+import sys
+import StringIO
+
+
+class ImageCheck:
+
+    def __init__(self, input_str):
+        self.input_str=input_str
+
+    def returnFilehandleFromInputStr(self):
+        if "http" in self.input_str:
+            self.urls = StringIO.StringIO(self.input_str)
+        else:
+            self.urls = open(self.input_str)
+        return self.urls
+
+    def returnDownloadImageCV2(self):
+      	self.resp = urllib.urlopen(self.url)
+	self.image = np.asarray(bytearray(self.resp.read()), dtype="uint8")
+	self.image = cv2.imdecode(self.image, cv2.IMREAD_COLOR)
+        return self.image
+
+    def goCheck(self):
+        for self.url in self.returnFilehandleFromInputStr():
+            self.filename = self.url.split("/")[-1]
+            print "downloading %s" % (self.url)
+            self.img = self.returnDownloadImageCV2()
+
+            self.aurl = 'http://104.199.154.201:5000/api/maria?url='+str(self.url)
+
+            self.r = urllib2.urlopen(self.aurl)
+            self.jsonData = json.loads(self.r.read())
+            self.r.close()
+            print json.dumps(self.jsonData, sort_keys=True, indent=4)
+
+            for self.list in self.jsonData['detect']:
+                cv2.rectangle(self.img,(self.list['right'],self.list['top']),(self.list['left'],self.list['bottom']),(0,255,0),3)
+                cv2.putText(self.img,self.list['class'],(self.list['left'],self.list['top']-10),cv2.FONT_HERSHEY_SIMPLEX, 1,(0, 0, 255),2)
+
+            self.dirname = "static/images/"
+            self.detectimage = "detect-"+self.filename
+            cv2.imwrite(os.path.join(self.dirname, self.detectimage), self.img)
+            return os.path.join(self.dirname, self.detectimage), json.dumps(self.jsonData, sort_keys=True, indent=4)
+
+        if returnFilehandleFromInputStr():
+            returnFilehandleFromInputStr().close()
+
+if __name__ == "__main__":
+    #ch = ImageCheck(sys.argv[1])
+    #ch.goCheck()
+    ImageCheck(sys.argv[1]).goCheck()
---
+++ app.py
@@ -0,0 +1,36 @@
+# coding: utf-8
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+import api
+
+app = Flask(__name__)
+
+def picked_up():
+    messages = "image check"
+    return messages
+
+@app.route('/')
+def index():
+    title = "check image url"
+    message = picked_up()
+    return render_template('index.html',
+                           message=message, title=title)
+
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+    title = "check image url"
+    #if request.method == 'POST':
+    if request.form['name']:
+        name = request.form['name']
+        detectname, detail = api.ImageCheck(name).goCheck()
+        return render_template('index.html',
+                               name=name, title=title, detectname=detectname, detail=detail)
+    else:
+        return redirect(url_for('index'))
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=3000)
